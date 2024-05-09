3908e58b67ce91640146ef8060b19113c39c3745
watermeloon <ifkovics.barnabas@gmail.com>
2024-05-09 23:23:53 +0200

    zig is wild

--- parser.zig
+++ parser.zig
@@ -1,6 +1,25 @@
 const std = @import("std");
+const expect = @import("std").testing.expect;
+const File = @import("std").fs.File;
+
+fn readBytes(file: File, num: u32) !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) std.debug.print("mem leak", .{});
+    }
+
+    const chunk = try allocator.alloc(u8, num);
+    defer allocator.free(chunk);
+
+    const ret: usize = try file.read(chunk);
+    std.debug.print("{any}b:{any}\n", .{ chunk, ret });
+}
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, {s}!\n", .{"world"});
+    const file = try std.fs.cwd().openFile("cat.jpeg", .{});
+    defer file.close();
+
+    _ = try readBytes(file, 8);
 }
