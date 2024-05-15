37035ee2711e5fa97873a40b5019dbefd3bdc6b0
watermeloon <ifkovics.barnabas@gmail.com>
2024-05-15 07:46:01 +0200

    refactor

--- parser.zig
+++ parser.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const expect = std.testing.expect;
 const File = std.fs.File;
+const Allocator = std.mem.Allocator;
 const AutoHashMap = std.hash_map.AutoHashMap;
 
 pub fn main() !void {
@@ -8,35 +9,22 @@ pub fn main() !void {
     defer file.close();
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator: std.mem.Allocator = gpa.allocator();
+    const allocator: Allocator = gpa.allocator();
     defer {
         const deinit_status = gpa.deinit();
         if (deinit_status == .leak) std.debug.print("mem leak", .{});
     }
 
-    const segment_parser: type = *const fn (file: File, allocator: std.mem.Allocator) anyerror!void;
+    const segment_parser: type = *const fn (file: File, allocator: Allocator) anyerror!void;
     var segment_map = AutoHashMap(u8, ?segment_parser).init(allocator);
     defer segment_map.deinit();
 
-    // Start of Image
     try segment_map.put(0xD8, startOfImage);
-
-    // Application 0
-    try segment_map.put(0xE8, null);
-
-    // Quantization Table
+    try segment_map.put(0xE0, app0);
     try segment_map.put(0xDB, quantizationTable);
-
-    // Start of Frame
     try segment_map.put(0xC0, startOfFrame);
-
-    // Huffman Table
     try segment_map.put(0xC4, huffmanTable);
-
-    // Start of Scan
     try segment_map.put(0xDA, startOfScan);
-
-    // End of Image
     try segment_map.put(0xD9, endOfImage);
 
     var marker: [2]u8 = undefined;
@@ -55,24 +43,23 @@ pub fn main() !void {
                 lagger = leader;
                 _ = try file.read(&leader);
             }
-
             try endOfImage(file, allocator);
             break;
         }
     }
 }
 
-fn markerNotFound(file: File, allocator: std.mem.Allocator) anyerror!void {
+fn markerNotFound(file: File, allocator: Allocator) anyerror!void {
     std.debug.print("WARNING: unknown marker\n", .{});
     _ = .{ file, allocator };
 }
 
-fn startOfImage(file: File, allocator: std.mem.Allocator) anyerror!void {
+fn startOfImage(file: File, allocator: Allocator) anyerror!void {
     std.debug.print("INFO: start of image\n", .{});
     _ = .{ file, allocator };
 }
 
-fn quantizationTable(file: File, allocator: std.mem.Allocator) anyerror!void {
+fn quantizationTable(file: File, allocator: Allocator) anyerror!void {
     std.debug.print("INFO: quantization table\n", .{});
 
     const length: u64 = try bytesAsDecimal(file, allocator, 2);
@@ -97,7 +84,7 @@ fn quantizationTable(file: File, allocator: std.mem.Allocator) anyerror!void {
     std.debug.print("INFO: table: {any}\n", .{table});
 }
 
-fn startOfFrame(file: File, allocator: std.mem.Allocator) anyerror!void {
+fn startOfFrame(file: File, allocator: Allocator) anyerror!void {
     std.debug.print("INFO: start of frame\n", .{});
 
     const length: u64 = try bytesAsDecimal(file, allocator, 2);
@@ -130,7 +117,7 @@ fn startOfFrame(file: File, allocator: std.mem.Allocator) anyerror!void {
     }
 }
 
-fn huffmanTable(file: File, allocator: std.mem.Allocator) anyerror!void {
+fn huffmanTable(file: File, allocator: Allocator) anyerror!void {
     std.debug.print("INFO: huffman table\n", .{});
 
     const length: u64 = try bytesAsDecimal(file, allocator, 2);
@@ -141,7 +128,7 @@ fn huffmanTable(file: File, allocator: std.mem.Allocator) anyerror!void {
     std.debug.print("INFO: table skipped: TODO\n", .{});
 }
 
-fn startOfScan(file: File, allocator: std.mem.Allocator) anyerror!void {
+fn startOfScan(file: File, allocator: Allocator) anyerror!void {
     std.debug.print("INFO: start of scan\n", .{});
 
     const length: u64 = try bytesAsDecimal(file, allocator, 2);
@@ -152,12 +139,23 @@ fn startOfScan(file: File, allocator: std.mem.Allocator) anyerror!void {
     std.debug.print("INFO: data skipped: TODO\n", .{});
 }
 
-fn endOfImage(file: File, allocator: std.mem.Allocator) anyerror!void {
+fn endOfImage(file: File, allocator: Allocator) anyerror!void {
     std.debug.print("INFO: end of image\n", .{});
     _ = .{ file, allocator };
 }
 
-fn bytesAsDecimal(file: File, allocator: std.mem.Allocator, sz: usize) !u64 {
+fn app0(file: File, allocator: Allocator) anyerror!void {
+    std.debug.print("INFO: application0\n", .{});
+
+    const length: u64 = try bytesAsDecimal(file, allocator, 2);
+    std.debug.print("INFO: length: {d}\n", .{length});
+
+    const data: []u8 = try readBytes(file, allocator, length - 2);
+    defer allocator.free(data);
+    std.debug.print("INFO: skipping app0: TODO\n", .{});
+}
+
+fn bytesAsDecimal(file: File, allocator: Allocator, sz: usize) !u64 {
     const length: []u8 = try readBytes(file, allocator, sz);
     defer allocator.free(length);
     return hexSliceToInt(length);
@@ -172,29 +170,8 @@ fn hexSliceToInt(bytes: []u8) u64 {
     return ret;
 }
 
-fn readBytes(file: File, allocator: std.mem.Allocator, sz: usize) ![]u8 {
+fn readBytes(file: File, allocator: Allocator, sz: usize) ![]u8 {
     const chunk: []u8 = try allocator.alloc(u8, sz);
     _ = try file.read(chunk);
     return chunk;
-}
-
-// archive
-fn readAPP0(file: File, allocator: std.mem.Allocator) !void {
-    const marker: []u8 = try readBytes(file, allocator, 2);
-    defer allocator.free(marker);
-    if (marker[0] != 0xFF and marker[1] != 0xE0) unreachable;
-
-    const length: []u8 = try readBytes(file, allocator, 2);
-    defer allocator.free(length);
-
-    const app0: []u8 = try readBytes(file, allocator, hexSliceToInt(length));
-    defer allocator.free(app0);
-
-    // validate indentifier      =>           .J    .F    .I    .F    /0
-    // if (!std.mem.eql(u8, &app0[0..5].*, &[_]u8{ 0x74, 0x70, 0x73, 0x70, 0x00 })) unreachable;
-    std.debug.print("identifier: {any}\nversion {d}.{d}\nunits {d}\n", .{ app0[0..5].*, app0[5], app0[6], app0[7] });
-    const density_x: []u8 = app0[8..10];
-    const density_y: []u8 = app0[10..12];
-    std.debug.print("density {d}x{d}\n", .{ hexSliceToInt(density_x), hexSliceToInt(density_y) });
-    std.debug.print("thumbnail {d}x{d}\n", .{ app0[12], app0[13] });
 }
